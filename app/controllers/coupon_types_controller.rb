969178a89d55ff2ee6c7d98cf7721ba5b4aae081
unknown <axelcampailla@hotmail.com>
2012-08-01 14:30:39 -0700

    init

--- app/controllers/coupon_types_controller.rb
+++ app/controllers/coupon_types_controller.rb
@@ -26,7 +26,7 @@ class CouponTypesController < ApplicationController
 		respond_to do |format|
 			#render text: "The Requested Coupon Type Does Not Exist.\n", status: 604
 			#format.json { render :json => "The Requested Coupon Type Does Not Exist.\n", status: 604 }
-			abort("TEST")
+			exit
 		end
 	else
 		respond_to do |format|
