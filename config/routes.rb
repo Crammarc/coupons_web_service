2b85fd666902c9dce608c901fb2374ba0fe06b18
unknown <axelcampailla@hotmail.com>
2012-08-01 15:31:57 -0700

    init

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Lelylan::Application.routes.draw do
   get "users/show" => "users#show"
   get "users/edit" => "users#edit"
   
-  get "coupon_types/not_found" => "coupon_types#not_found"
+  get "not_found" => "coupon_types#not_found"
 
   resources :users
   resources :sessions
